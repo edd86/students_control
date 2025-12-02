68e382dad63a84f387c5c6ac6c59dc01de739952
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-12-01 23:33:46 -0400

    feat: Add routes for CourseDetailsPage and StudentsRegisterPage in app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:students_control/features/courses/presentation/pages/course_details_page.dart';
 import 'package:students_control/features/courses/presentation/pages/course_register_page.dart';
 import 'package:students_control/features/login/presentation/pages/login_page.dart';
 import 'package:students_control/features/teachers/presentation/pages/teacher_register_page.dart';
+import 'package:students_control/features/students/presentation/pages/students_register_page.dart';
 import 'package:students_control/features/dashboard/presentation/pages/dashboard_page.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
@@ -21,6 +23,17 @@ final appRouterProvider = Provider<GoRouter>((ref) {
       GoRoute(
         path: '/register_course',
         builder: (context, state) => const CourseRegisterPage(),
+      ),
+      GoRoute(
+        path: '/course_details/:id',
+        builder: (context, state) {
+          final id = state.pathParameters['id']!;
+          return CourseDetailsPage(courseId: id);
+        },
+      ),
+      GoRoute(
+        path: '/register_student',
+        builder: (context, state) => const StudentsRegisterPage(),
       ),
     ],
   );
